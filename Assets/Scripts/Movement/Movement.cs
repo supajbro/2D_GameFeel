59371e1b3b31cb6b29752fc52c4a7100af1b39ea
supajbro <supajbro@gmail.com>
2025-04-03 22:35:59 +1000

    Adding jump functionality

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
@@ -13,30 +10,64 @@ public class Movement : MonoBehaviour
         Idle = 0,
         Walk,
         Jump,
+        Fall,
         Shoot
     }
     [SerializeField] private CharacterStates m_currentState;
+    [SerializeField] private CharacterStates m_previousState;
+    private void SetState(CharacterStates state)
+    {
+        m_previousState = m_currentState;
+        m_currentState = state;
+    }
 
     [Header("Main Components")]
     [SerializeField] private PlayerControls m_controls;
     private CharacterController m_controller;
 
-    [Header("Values")]
+    [Header("Movement Values")]
     [SerializeField] private float m_speed = 5.0f;
-    [SerializeField] private float m_YVelocity = 0.0f;
-    private float m_moveInput = 0.0f;
+
+    [Header("Jump Values")]
+    [SerializeField] private float m_jumpHeight = 5.0f;
+    [SerializeField] private float m_currentJumpHeight = 0.0f;
+    [SerializeField] private float m_minJumpHeight = 2.0f;
+    [SerializeField] private float m_maxJumpHeight = 10.0f;
+    [SerializeField] private float m_maxJumpTimer = 1.0f;
+    [SerializeField] private float m_jumpHeightIncreaseSpeed = 5.0f;
+    [SerializeField] private float m_jumpHeightDecreaseSpeed = 10.0f;
+    private float m_jumpTimer = 0.0f;
+
+    [Header("Fall Values")]
+    [SerializeField] private float m_fallDelay = 0.0f;
+    [SerializeField] private float m_maxFallDelay = 0.1f;
+
+    [Header("Hidden Values")]
+    private Vector2 m_moveInput = Vector2.zero;
+    private float m_xMovement = 0.0f;
+    private float m_yMovement = 0.0f;
+    private Vector3 m_playerPos = Vector3.zero;
+
+    [Header("Const Values")]
+    private const float GroundRayLength = 0.55f;
 
     private void Awake()
     {
         m_controls = new PlayerControls();
         m_controller = GetComponent<CharacterController>();
+        SetState(CharacterStates.Idle);
     }
 
     private void OnEnable()
     {
         m_controls.Enable();
-        m_controls.Player.Move.performed += ctx => m_moveInput = ctx.ReadValue<float>();
-        m_controls.Player.Move.canceled += ctx => m_moveInput = 0;
+
+        // Walking
+        m_controls.Player.Move.performed += ctx => m_moveInput.x = ctx.ReadValue<float>();
+        m_controls.Player.Move.canceled += ctx => m_moveInput.x = 0;
+
+        // Jumping
+        m_controls.Player.Jump.performed += ctx => PressedJump();
     }
 
     private void OnDisable()
@@ -44,15 +75,163 @@ public class Movement : MonoBehaviour
         m_controls.Disable();
     }
 
-    private void Update()
+    private void StateUpdate()
     {
-        MovementUpdate();
+        switch(m_currentState)
+        {
+            case CharacterStates.Idle:
+                IdleUpdate();
+                break;
+
+            case CharacterStates.Walk:
+                MovementUpdate();
+                break;
+
+            case CharacterStates.Jump:
+                JumpUpdate();
+                break;
+
+            case CharacterStates.Fall:
+                FallUpdate();
+                break;
+        }
+    }
+
+    private void IdleUpdate()
+    {
+        if(m_moveInput.x != 0 && IsGrounded())
+        {
+            SetState(CharacterStates.Walk);
+        }
+        else if (!IsGrounded())
+        {
+            SetState(CharacterStates.Fall);
+        }
+
+        ResetJumpStats();
     }
 
     private void MovementUpdate()
     {
-        Vector3 playerPos = new Vector3(m_moveInput * m_speed * Time.deltaTime, m_YVelocity, 0);
-        m_controller.Move(playerPos);
+        if(m_moveInput.x == 0 && IsGrounded())
+        {
+            SetState(CharacterStates.Idle);
+        }
+        else if (!IsGrounded())
+        {
+            SetState(CharacterStates.Fall);
+        }
+
+        m_xMovement = m_moveInput.x * m_speed * Time.deltaTime;
+
+        ResetJumpStats();
+    }
+
+    private void JumpUpdate()
+    {
+        if (m_jumpTimer > 0.5f)
+        {
+            if (m_moveInput.x == 0 && IsGrounded())
+            {
+                SetState(CharacterStates.Idle);
+                m_yMovement = 0.0f;
+                return;
+            }
+            else if (IsGrounded())
+            {
+                SetState(CharacterStates.Walk);
+                m_yMovement = 0.0f;
+                return;
+            }
+        }
+
+        m_jumpTimer += Time.deltaTime;
+
+        if(m_jumpTimer > m_maxJumpTimer)
+        {
+            m_fallDelay += Time.deltaTime;
+            if (m_fallDelay > m_maxFallDelay)
+            {
+                SetState(CharacterStates.Fall);
+            }
+            return;
+        }
+
+        m_currentJumpHeight = (m_currentJumpHeight < m_maxJumpHeight) ? m_currentJumpHeight + (Time.deltaTime * m_jumpHeightIncreaseSpeed) : m_maxJumpHeight;
+        m_moveInput.y += m_currentJumpHeight * Time.deltaTime;
+
+        m_yMovement = m_moveInput.y * m_jumpHeight;
+
+        // Allow x movement in the air
+        m_xMovement = m_moveInput.x * m_speed * Time.deltaTime;
+    }
+
+    private void FallUpdate()
+    {
+        if (m_moveInput.x == 0 && IsGrounded())
+        {
+            SetState(CharacterStates.Idle);
+            m_yMovement = 0.0f;
+            return;
+        }
+        else if (IsGrounded())
+        {
+            SetState(CharacterStates.Walk);
+            m_yMovement = 0.0f;
+            return;
+        }
+
+        m_currentJumpHeight = (m_currentJumpHeight > m_minJumpHeight) ? m_currentJumpHeight - (Time.deltaTime * m_jumpHeightDecreaseSpeed) : m_minJumpHeight;
+        m_moveInput.y -= m_currentJumpHeight * Time.deltaTime;
+
+        m_yMovement = m_moveInput.y * m_jumpHeight;
+
+        // Allow x movement in the air
+        m_xMovement = m_moveInput.x * m_speed * Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        IsGrounded();
+        StateUpdate();
+
+        m_playerPos = new Vector2(m_xMovement, m_yMovement);
+        m_controller.Move(m_playerPos);
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawRay(transform.position, Vector2.down * GroundRayLength, Color.red);
+
+        if (Physics.Raycast(transform.position, Vector3.down, GroundRayLength))
+        {
+            Debug.Log("Hit");
+            return true;
+        }
+
+        return false;
+    }
+
+    private void PressedJump()
+    {
+        if (!IsGrounded())
+        {
+            return;
+        }
+
+        m_currentJumpHeight = m_minJumpHeight;
+        m_jumpTimer = 0.0f;
+        m_fallDelay = 0.0f;
+        SetState(CharacterStates.Jump);
+    }
+
+    private void ResetJumpStats()
+    {
+        m_yMovement = 0.0f;
+        m_moveInput.y = 0.0f;
+        m_jumpTimer = 0.0f;
+        m_currentJumpHeight = 0.0f;
+        m_fallDelay = 0.0f;
     }
 
 }
