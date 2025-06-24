6f6e672ee70c813af3ddf12c45a1a418b95e5736
supajbro <supajbro@gmail.com>
2025-06-24 22:45:25 +1000

    Removing unused libraries

--- Assets/Enemy/Scripts/EnemyFlyTo.cs
+++ Assets/Enemy/Scripts/EnemyFlyTo.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyFlyTo : Enemy, IEnemy, IHealth
--- Assets/Scripts/IHealth.cs
+++ Assets/Scripts/IHealth.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public interface IHealth
 {
     public float CurrentHealth {  get; set; }
